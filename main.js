020ee0130a1269c7d4d24351ef76beb09184c743
Irina Kurilenko <irina.kurilenk@rambler.ru>
2020-09-25 13:34:22 +0300

    Homework-2 part 2

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const secondRow = 'собака друг человека';
 function getNumberOfItems(item, someString) {
 	let counter = 0;
 	for (i = 0; i < someString.length; i++) {
-		if (someString.charAt(i) == item) {
+		if (someString.charAt(i) === item) {
 			counter++;
 		}
 	}
@@ -14,14 +14,21 @@ function getNumberOfItems(item, someString) {
 //console.log(getNumberOfItems('а', firstRow));
 //console.log(getNumberOfItems('а', secondRow));
 
-function getRow(firstRow, secondRow) {
-	if (getNumberOfItems("а", firstRow) >= getNumberOfItems("а", secondRow)) {
-		return firstRow;
-	} else {
-		return secondRow;
-	}
-}
-
+const getRow = (firstRow, secondRow) =>
+	getNumberOfItems("а", firstRow) >= getNumberOfItems("а", secondRow) ? firstRow : secondRow;
 
 console.log(getRow(firstRow, secondRow)); // мама мыла раму
 
+/* =======================================================================*/
+
+let phone = '+71234567890';
+//console.log(phone.length);
+
+function formattedPhone(phone) {
+	for (i = 0; i < phone.length; i++) {
+		phone.charAt(i);
+	}
+	return (`${phone[0] + phone[1]} (${phone[2]}${phone[3]}${phone[4]}) ${phone[5]}${phone[6]}${phone[7]}-${phone[8]}${phone[9]}-${phone[10]}${phone[1]}`);
+}
+
+console.log(formattedPhone('+71234567890'));
